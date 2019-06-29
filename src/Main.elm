815c1353439d8e60d409a2c02c59a651643bb2b4
James Rakich <james@fullandbydesign.com.au>
2019-06-29 17:13:51 +1000

    Improve data structure.

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,22 @@
 import Svg exposing (..)
-import Svg.Attributes exposing (..)
+import Svg.Attributes exposing (x1, y1, x2, y2, stroke, fill, width, height, viewBox, points)
 import String
 import List
 
+type alias Vector2D = 
+  { x : Float
+  , y : Float
+  }
 
+type alias Vector3D = 
+  { x : Float
+  , y : Float
+  , z : Float
+  }
+
+
+translate2d : Float -> Float -> Vector2D -> Vector2D
+translate2d x y pos = {x = pos.x + x, y = pos.y + y}
 
 boat_coords =
   [ [0, 0]
@@ -18,29 +31,25 @@ boat_coords =
   , [0, 0]
   ]
 
+boat_shape = List.map list_to_vector2d boat_coords
 
-coord_to_string_point : List Float -> String
-coord_to_string_point coords = String.join "," (List.map String.fromFloat coords)
+list_to_vector2d : List Float -> Vector2D
+list_to_vector2d floats = case floats of
+  [] -> {x = 0, y = 0}
+  x1 :: y1 :: rest -> {x = x1, y = y1}
+  x1 :: rest -> {x = x1, y = 0}
 
-coords_to_string_points : List (List Float) -> String
+underwater_polygon : List Vector2D -> List Vector2D
+underwater_polygon points = List.map (\p -> {x = p.x, y = min p.y 250}) points
+
+coord_to_string_point : Vector2D -> String
+coord_to_string_point pos = String.join "," [String.fromFloat pos.x, String.fromFloat pos.y]
+
+coords_to_string_points : List Vector2D -> String
 coords_to_string_points coords = String.join " " (List.map coord_to_string_point coords)
 
-
-translate : Float -> Float -> List Float -> List Float
-translate x y coords = case coords of
-  [] -> [0 + x, 0 + y]
-  x1 :: y1 :: rest -> [x1 + x, y1 + y]
-  x1 :: rest -> [x1 + x, 0 + y]
-
-flip_vertical : List Float -> List Float
-flip_vertical coords = case coords of
-  [] -> [0 * 1, 0 * -1]
-  x1 :: y1 :: rest -> [x1 * 1, y1 * -1]
-  x1 :: rest -> [x1 * 1, 0 * -1]
-
-
-boatTranslate = translate 250 340
-
+flip_vertical : Vector2D -> Vector2D
+flip_vertical pos = {x = pos.x * 1, y = pos.y * -1}
 
 type Msg
   = Tick
@@ -77,7 +86,7 @@ view model =
         , stroke "blue"
         ][]
     , polyline
-        [ points (coords_to_string_points (List.map (\c -> (translate model.pos.x model.pos.y (flip_vertical c))) boat_coords))
+        [ points (coords_to_string_points (List.map (\c -> (translate2d model.pos.x model.pos.y (flip_vertical c))) boat_shape))
         , fill "green"
         , stroke "red"
         ][]
